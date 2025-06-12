028204de1de146c4439a215c723bb2267794e162
Florentin Vasilica Camasa <Sile_dynny@yahoo.com>
2025-06-12 20:39:46 +0200

    primer commit del fichero index

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//todos los imports necesarios 
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config(); 
+
+const router = require("./routes/api.routes");
+
+// crear un servidor y configuracion
+const server = express();
+server.use(express.json());
+server.use('/', router);
+server.use(cors());
+
+server.use("/api", router);
+
+// puerto a traves de cual escucho
+const PORT = 3500;
+server.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
+
+
