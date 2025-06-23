a29002103f3af84dfe6b2e88f8f363abc207c446
Rosa Vela <rosymer96@gmail.com>
2025-06-23 11:01:09 +0200

    Rutas menu y note  con autorizacion de token y rol probadas

--- src/routes/api_routes/menu.routes.js
+++ src/routes/api_routes/menu.routes.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
 const menuCon = require("../../controllers/menu.controllers");
+const { checkToken, authorizeRoles } = require("../../middleware/auth");
+
+// Aplica el middleware a todas las rutas de nota de menú
+router.use(checkToken, authorizeRoles("tutor")); // solo tutores pueden acceder
 
 //Crear el menu asignandole 3 platos, fecha y la clase.
 
 router.post("/create", menuCon.createMenu);
 
 //Listar menu por clase recibe un queryparams
-router.get("/list/:classId", menuCon.listByClassMonth);
+router.get("/listByClass/:classId", menuCon.listByClassMonth);
 //Editar el menu
 router.put("/:menuId", menuCon.updateMenu);
 //Borrar Menu
 router.delete("/:menuId", menuCon.deleteMenu);
+
 module.exports = router;
